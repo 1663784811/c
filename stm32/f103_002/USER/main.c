54b2ef693a5e990efcd4ddd9c066551c733ea0b6
why <1663784811@qq.com>
2022-05-04 02:08:43 +0800

    init

--- stm32/f103_002/USER/main.c
+++ stm32/f103_002/USER/main.c
@@ -1,7 +1,44 @@
 #include "stm32f10x.h"
+#include "stm32_eval.h"
+#include <stdio.h>
+
+
+void usartOut(void);
+
 
 int main(void){
-    
+
+
+    usartOut();
+
+
+
+
+
+
+
+
+}
+
+
+/**
+ * 串口输出
+ */
+void usartOut(void){
+
+    // 波特率
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+    STM_EVAL_COMInit(COM1, &USART_InitStructure);
+
+
+
+
 }
 
 
