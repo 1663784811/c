e6e16b7098f243e0cec28ccb3c466199e6eaf08c
why <1663784811@qq.com>
2022-05-04 20:17:26 +0800

    init

--- stm32/f103_002/USER/main.c
+++ stm32/f103_002/USER/main.c
@@ -1,23 +1,58 @@
-#include "stm32f10x.h"
-#include "stm32_eval.h"
 #include <stdio.h>
+#include "stm32f10x.h"
 
+
+/**
+ * =============================    宏定义
+ */
+#define COMn                             1
+#define EVAL_COM1_TX_GPIO_CLK            RCC_APB2Periph_GPIOD
+
+#define EVAL_COM1                        USART2
+#define EVAL_COM1_CLK                    RCC_APB1Periph_USART2
+#define EVAL_COM1_TX_PIN                 GPIO_Pin_5
+#define EVAL_COM1_TX_GPIO_PORT           GPIOD
+#define EVAL_COM1_TX_GPIO_CLK            RCC_APB2Periph_GPIOD
+#define EVAL_COM1_RX_PIN                 GPIO_Pin_6
+#define EVAL_COM1_RX_GPIO_PORT           GPIOD
+#define EVAL_COM1_RX_GPIO_CLK            RCC_APB2Periph_GPIOD
+#define EVAL_COM1_IRQn                   USART2_IRQn
+
+
+typedef enum
+{
+    COM1 = 0,
+    COM2 = 1
+} COM_TypeDef;
+
+USART_TypeDef* COM_USART[COMn] = {EVAL_COM1};
+GPIO_TypeDef* COM_TX_PORT[COMn] = {EVAL_COM1_TX_GPIO_PORT};
+GPIO_TypeDef* COM_RX_PORT[COMn] = {EVAL_COM1_RX_GPIO_PORT};
+
+
+const uint32_t COM_TX_PORT_CLK[COMn] = {EVAL_COM1_TX_GPIO_CLK};
+const uint32_t COM_USART_CLK[COMn] = {EVAL_COM1_CLK};
+const uint32_t COM_RX_PORT_CLK[COMn] = {EVAL_COM1_RX_GPIO_CLK};
+const uint16_t COM_TX_PIN[COMn] = {EVAL_COM1_TX_PIN};
+const uint16_t COM_RX_PIN[COMn] = {EVAL_COM1_RX_PIN};
+
+
+// 变量
+USART_InitTypeDef USART_InitStructure;
+
+/**
+ * =============================   函数定义
+ */
 
 void usartOut(void);
-
-
+void STM_EVAL_COMInit(COM_TypeDef COM, USART_InitTypeDef* USART_InitStruct);
+/**
+ * =============================    主启动类
+ */
 int main(void){
-
-
-    usartOut();
-
-
-
-
-
-
-
-
+	
+  usartOut();
+	return 0;
 }
 
 
@@ -25,7 +60,6 @@ int main(void){
  * 串口输出
  */
 void usartOut(void){
-
     // 波特率
     USART_InitStructure.USART_BaudRate = 115200;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
@@ -33,12 +67,50 @@ void usartOut(void){
     USART_InitStructure.USART_Parity = USART_Parity_No;
     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
     STM_EVAL_COMInit(COM1, &USART_InitStructure);
 
 
-
-
 }
+
+
+void STM_EVAL_COMInit(COM_TypeDef COM, USART_InitTypeDef* USART_InitStruct){
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  /* Enable GPIO clock */
+  RCC_APB2PeriphClockCmd(COM_TX_PORT_CLK[COM] | COM_RX_PORT_CLK[COM] | RCC_APB2Periph_AFIO, ENABLE);
+
+
+  /* Enable UART clock */
+  if (COM == COM1)
+  {
+    RCC_APB2PeriphClockCmd(COM_USART_CLK[COM], ENABLE); 
+  }
+  else
+  {
+    /* Enable the USART2 Pins Software Remapping */
+    GPIO_PinRemapConfig(GPIO_Remap_USART2, ENABLE);
+    RCC_APB1PeriphClockCmd(COM_USART_CLK[COM], ENABLE);
+  }
+
+  /* Configure USART Tx as alternate function push-pull */
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_InitStructure.GPIO_Pin = COM_TX_PIN[COM];
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(COM_TX_PORT[COM], &GPIO_InitStructure);
+
+  /* Configure USART Rx as input floating */
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_InitStructure.GPIO_Pin = COM_RX_PIN[COM];
+  GPIO_Init(COM_RX_PORT[COM], &GPIO_InitStructure);
+
+  /* USART configuration */
+  USART_Init(COM_USART[COM], USART_InitStruct);
+    
+  /* Enable USART */
+  USART_Cmd(COM_USART[COM], ENABLE);
+}
+
+
+
 
 
