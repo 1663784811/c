0ec103cf4dccba2942d3e2884d5a44318eb9d217
why <xxxxxx@xxx.xxx>
2023-05-10 23:33:25 +0800

    init

--- opencv/test01/main.cpp
+++ opencv/test01/main.cpp
@@ -1,11 +1,17 @@
+#include <iostream>
 #include <opencv2/opencv.hpp>
+
+using namespace std;
 using namespace cv;
 
 int main() {
-    Mat image = imread("D:\\a.jpg");
-    imshow("Image", image);
+    Mat srcImage = imread("D:\\a.jpg");
+    if (!srcImage.data) {
+        std::cout << "Image not loaded";
+        return -1;
+    }
+    imshow("image", srcImage);
     waitKey(0);
-    destroyAllWindows();
-    printf("ssssssssssssssssss");
+    printf("sssssssssssssssssssssssss");
     return 0;
 }
