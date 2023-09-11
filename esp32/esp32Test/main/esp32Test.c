0b3907c0d3135f4d7ec09ad50421c726b4ce4654
why <1663784811@qq.com>
2023-09-11 16:40:45 +0800

    esp32

--- esp32/esp32Test/main/esp32Test.c
+++ esp32/esp32Test/main/esp32Test.c
@@ -6,10 +6,23 @@
 #include "freertos/task.h"
 #include "esp_chip_info.h"
 #include "esp_flash.h"
+//#include "lcd/lcd_init.h"
+
+
+void app_main(void) {
+
+    // ==========================  初始化
+
+//    LCD_GPIO_Init();
 
 
 
-void app_main(void)
-{
+    // ==========================  任务
+    // 显示
+
+
+
+
+    // 启动任务
 
 }
